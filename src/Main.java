a8984191441a4bfd21f9e8242678c2eb4cb8463d
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-08-07 22:42:54 -0400

    my first java code, learned from youtube tutorials

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+// Class
+public class Main {
+
+    // Method and entry point for program
+    public static void main(String[] args) {
+        System.out.println("Hooray my first java app");
+
+        // Comment
+
+        /* Multi
+        * line
+        * comment */
+
+        // Integer
+        int a = 5;
+
+        // Character
+        char b = 'b';
+
+        // Long
+        long c = 200;
+
+        // String - not a primitive, is an object
+        String name = "Zach";
+
+        boolean x = true;
+
+
+
+    }
+}
