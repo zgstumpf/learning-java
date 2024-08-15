31afb18336eb3b21d75760efd8f88289499a898f
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-08-15 18:50:04 -0500

    final method

--- src/oop/modifiers/package3/Class1.java
+++ src/oop/modifiers/package3/Class1.java
@@ -4,4 +4,9 @@ class Class1 {
     // Order of modifiers doesn't matter.
     final static int x = 2;
     static final int y = 3;
+
+    // Any class that extends Class1 can't override this function.
+    final void myFunction() {
+
+    }
 }
---
+++ src/oop/modifiers/package3/Class2.java
@@ -0,0 +1,8 @@
+package oop.modifiers.package3;
+
+class Class2 extends Class1 {
+    // This errors because Class1 has myFunction set to final.
+//    void myFunction() {
+//
+//    }
+}
