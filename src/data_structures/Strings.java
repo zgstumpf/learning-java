24e3da86d07902e5a5e0859fa7bdb5103f018475
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-08-16 18:51:31 -0500

    clearing up knowledge on strings

--- src/data_structures/Strings.java
+++ src/data_structures/Strings.java
@@ -6,11 +6,21 @@ public class Strings {
 
         // Length
         // Be careful - .length() is used for String, .length is used for Array.
-        assert str.length() == 6;
+        System.out.println(str.length() == 6); // true
 
         // Slicing
-        assert str.substring(0, 1).equals("a");
-        assert str.substring(1, 4).equals("bcd");
-        assert str.substring(str.length() - 1).equals("f");
+        // -------
+        // Strings are 0-indexed, second argument of substring is STOP index.
+        System.out.println(str.substring(0, 1).equals("a")); // true
+        System.out.println(str.substring(1, 4).equals("bcd")); // true
+        // If STOP index argument is left out, it defaults to end of String.
+        // Can't use -1 to get last index like in Python
+        System.out.println(str.substring(str.length() - 1).equals("f")); // true
+        System.out.println(str.substring(1).equals("bcdef")); // true
+
+        // Remember to always use .equals(), since == compares memory locations
+        System.out.println(str.substring(0, 1) == "a"); // false
+        // This isn't true because str.substring() is NOT a String literal, while "a" is.
+        // See IdentityVsEquality.java for more info
     }
 }
