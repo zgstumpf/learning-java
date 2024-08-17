889aba2944da04465667a2910b9fbf160313382c
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-08-17 13:45:55 -0500

    string operations

--- src/data_structures/Strings.java
+++ src/data_structures/Strings.java
@@ -1,5 +1,7 @@
 package data_structures;
 
+import java.util.Arrays;
+
 public class Strings {
     public static void main(String[] args) {
         String str = "abcdef";
@@ -22,5 +24,40 @@ public class Strings {
         System.out.println(str.substring(0, 1) == "a"); // false
         // This isn't true because str.substring() is NOT a String literal, while "a" is.
         // See IdentityVsEquality.java for more info
+
+        // Concatenate strings
+        String str1 = "abc";
+        String str2 = "def";
+        String str3 = str1 + str2;
+        System.out.println(str3); // "abcdef"
+
+        // contains
+        System.out.println(str.contains("bc")); // true
+
+        // replace
+        String newStr = str.replace("a", "z");
+        System.out.println(newStr); // "zbcdef"
+
+        // trim
+        String whitespaceStr = "  so. much. space.  ";
+        String trimmedStr = whitespaceStr.trim();
+        System.out.println(trimmedStr); // "so. much. space."
+
+        // lowercase, uppercase
+        String caps = "ABC";
+        String lowered = caps.toLowerCase();
+        System.out.println(lowered);
+        String uppered = lowered.toUpperCase();
+        System.out.println(uppered);
+
+        // split
+        String sentence = "This is a sentence with multiple spaces.";
+        String[] words = sentence.split(" ");
+        System.out.println(Arrays.toString(words) + " length: " + words.length);
+
+        // join
+        String[] words2 = {"combine", "these", "words"};
+        String sentence2 = String.join(" ", words2);
+        System.out.println(sentence2);
     }
 }
