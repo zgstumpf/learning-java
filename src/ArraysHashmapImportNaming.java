bf83a9cf6cd78e2d45fed69d87d90329cf45c703
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-08-18 16:55:02 -0500

    way to remember arrays imports'

---
+++ src/ArraysHashmapImportNaming.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.HashMap; // Note capitalization
+
+// Why is Arrays plural and HashMap singular?
+// Because Arrays is a class, not an array data structure, that gives many operations on Arrays,
+// but HashMap is a class that IS a hashmap data structure.
+
+public class ArraysHashmapImportNaming {
+}
