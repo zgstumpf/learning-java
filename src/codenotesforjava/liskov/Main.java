b4e9b1c7890fd8034909fd17a246ec4c83a3f401
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-08-21 13:24:51 -0500

    overcomplicated polymorhpism stuff

--- src/codenotesforjava/liskov/Main.java
+++ src/codenotesforjava/liskov/Main.java
@@ -16,10 +16,19 @@ class Main {
         convertible0.drive();
         convertible0.activateRoof();
 
+        // This section includes polymorphism concepts.
+        // --------------------------------------------
         // Why does this work?
-        Car convertible1 = new Convertible(); // Subclass used in place of its superclass.
+        Car convertible1 = new Convertible();
+        // Subclass used in place of its superclass.
+        // Create Convertible object, store reference in Car variable.
+        // This only works because Convertible is a type of Car.
+        // Because convertible1 has been declared as a Car, it only has access to Car fields/methods.
+
+
         // But not this?
         // Convertible convertible2  = new Car(); // Error - superclass used in place of its subclass.
+        // Doesn't work because Convertible may have additional fields/methods not defined in Car class.
 
         // convertible1.activateRoof() // Error - Because convertible1 is actually type Car, it
         // does not have the activateRoof method.
