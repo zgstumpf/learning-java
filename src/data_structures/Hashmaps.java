a8b134c93b26bc078e54c76ab113e0acb16f3182
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-08-29 16:13:02 -0400

    hashmap

---
+++ src/data_structures/Hashmaps.java
@@ -0,0 +1,37 @@
+package data_structures;
+
+import java.util.HashMap;
+
+public class Hashmaps {
+    public static void main(String[] args) {
+        // Declare and initialize hashmap
+        HashMap<String, Integer> map = new HashMap<>();
+
+        // Add keys/values
+        map.put("a", 1);
+        map.put("b", 5);
+
+        // Get value based on key
+        System.out.println(map.get("b")); // 5
+
+        // Print hashmap
+        System.out.println(map); // {a=1, b=5}
+
+        // Is key in hashmap
+        System.out.println(map.containsKey("b")); // true
+        // Is value in hashmap
+        map.containsValue(6); // true
+
+        // Modify value of key - 2 examples
+        map.put("b", map.get("b") + 1);
+        map.put("b", 8);
+        System.out.println(map);
+
+        // Get length (number of keys)
+        System.out.println(map.size()); // 2
+
+        // Remove key-value pair
+        map.remove("a");
+        System.out.println(map);
+    }
+}
